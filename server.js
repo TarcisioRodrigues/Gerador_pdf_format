61630ad8635f1b3699df15a22c9b444b2cd3f690
TarcisioRodrigues <tarcisiorodrigues454@gmail.com>
2022-08-12 21:57:30 -0300

    part pdf :(

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from "express";
 import bodyParser from "body-parser";
 import path from 'path'
 import ejs from 'ejs'
+import fs from 'fs'
+
 import puppeteer from 'puppeteer'
 
 
@@ -13,36 +15,28 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.set('views',path.join(__dirname,'/views'))
 // app.use(app);
+import pdf from 'html-pdf'
 
-
-app.get('/pdf', async(request, response) => {
-
-
-    const browser = await puppeteer.launch()
-    const page = await browser.newPage()
-
-    await page.goto('http://localhost:3333/', {
-        waitUntil: 'networkidle0'
-    })
-
-    const pdf = await page.pdf({
-        printBackground: true,
-        format: 'Letter'
-    })
-
-    await browser.close()
-
-    response.contentType("application/pdf")
-
-    return response.send(pdf)
-
-})
+ const GeneratePDF=(response,request) => {
+    const html = path.join(__dirname, "views/print.ejs")
+    ejs.renderFile(html, (err, data) => {
+        const options={
+            type:'pdf',
+            format:'A4',
+            orientation: 'portrait'
+        }
+          pdf.create(data, options).toFile("./Reports/report.pdf");
+      
+        })
+}
+app.get('/pdf',GeneratePDF)
 app.post('/', (request, response) => {
     console.log(request.body)
     const {Nome,Adress,data,CEP,Entreprise,CNPJ}=request.body
     console.log(Nome)
     const filePath = path.join(__dirname, "views/print.ejs")
     ejs.renderFile(filePath, { Nome,Adress,data,CEP,Entreprise,CNPJ}, (err, html) => {
+        
         if(err) {
             return response.send('Erro na leitura do arquivo')
         }
